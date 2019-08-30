fd6f8b517bed313e0d6136ba2c9207ca1e1ad46b
Tyler Corwin <Tyler.C@figmints.com>
2019-08-30 11:42:34 -0400

    WIP added styles, button for new ideas, and connection between react and rails

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -1,9 +1,52 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import Idea from './Idea'
+import update from 'immutability-helper'
+import IdeaForm from './IdeaForm'
 
 class IdeasContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      ideas: [],
+      editingIdeaId: null
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3000/api/v1/ideas.json')
+    .then(response => {
+      this.setState({ideas: response.data})
+    })
+    .catch(error => console.log(error))
+  }
+
+  addNewIdea = () => {
+    axios.post('http://localhost:3000/api/v1/ideas', {idea: {title: '', body: ''}})
+    .then(response => {
+      // this.setState({ideas: response.data})
+      console.log(response)
+      const ideas = update(this.state.ideas, { $splice: [[0, 0, response.data]]})
+      this.setState({ideas: ideas, editingIdeaId: response.data.id})
+    })
+    .catch(error => console.log(error))
+  }
+
   render() {
     return (
-      <div className="">
+      <div>
+        <div>
+          <button className="newIdeaButton" onClick={this.addNewIdea}>
+            New Idea
+          </button>
+        </div>
+        {this.state.ideas.map((idea) => {
+          if (this.state.editingIdeaId === idea.id) {
+            return(<IdeaForm idea={idea} key={idea.id} />)
+          } else {
+            return (<Idea idea={idea} key={idea.id} />)
+          }
+        })}
       </div>
     );
   }
